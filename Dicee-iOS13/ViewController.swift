510cb630f1182f5d7dc5dfaad7652d8ad6922629
i499804 <i499804@fontysict.nl>
2024-05-16 22:06:10 +0200

    Rolling dice - Swift project

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // The connected image from the storyboard (ctrl + drag).
+        
+        diceImageView1.image = #imageLiteral (resourceName: "DiceSix")
+    
+        
+        // Second process of the ctrl + drag image.
+        
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
+        
     }
 
-
+    // Once the button gets pressed, two fours appear.
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
+    
 }
 
